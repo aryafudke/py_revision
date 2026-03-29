3c06417aae0eb809922f7edb00bc7cb5b1114713
aryafudke <aryafudke@gmail.com>
2026-03-29 12:47:10 +0530

    Complete Topic 1: Variables & Data Types

---
+++ new_day_1.py
@@ -0,0 +1,20 @@
+# create 5 varibales of diff types, print each with type()
+
+name = "Arya Fudke"
+age = 24
+salary = 100000.00
+is_working = True 
+middle_name = None 
+
+print (type(name))
+print (type(age))
+print (type(salary))
+print (type(is_working))
+print (type(middle_name))
+print(f"My name is {name} and I am {age} years old, i earn {salary} per month")
+# twist part
+
+fav_food = "Sushi"
+price = 500.50
+print(f"My fav food is {fav_food}, which costs arround {price}rs")
+print(f"string - {type(fav_food)}, int - {type(age)}, decimal - {type(price)}, boolean - {type(is_working)}, none - {type(middle_name)}")
